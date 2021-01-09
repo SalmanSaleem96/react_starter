c5d1783410cc242e853edf7b6af773b0fd8b2f32
Salman Saleem <salmansaleem24689@gmail.com>
2021-01-09 17:48:39 +0500

    React Assignment Using Props

---
+++ src/routine.js
@@ -0,0 +1,10 @@
+import React from 'react';
+function Routine(props) {
+    return (
+        <div>
+
+            <h2>Izhan goto {props.education} at {props.time}</h2>
+        </div>
+    )
+}
+export default Routine;
